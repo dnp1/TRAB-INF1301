39da324c43395b4774f49364c00d0fdca1e764e9
eaverdeja <eaverdeja@gmail.com>
2014-10-22 01:11:30 -0200

    Criar Tabuleiro

--- t3/TABULEIRO.c
+++ t3/TABULEIRO.c
@@ -1,3 +1,29 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: TAB  Tabuleiro de labirinto
+*
+*  Arquivo gerado:              TABULEIRO.c
+*  Letras identificadoras:      TAB
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\TABULEIRO.BSW
+*
+*  Projeto: INF 1301 / Labirinto
+*  Gestor:  LSKDANEAV
+*  Autores: eav
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*		 1	   eav	 20/10/2014		//
+*
+***************************************************************************/
+#include    <string.h>
+#include    <stdio.h>
+#include    <malloc.h>
+
+#define TABULEIRO_OWN
+#include "TABULEIRO.h"
+#undef TABULEIRO_OWN
+
 struct Casa{
     int valor;//1 vazio, 2 parede, 3 inicio, 4 final
     int norte;//ids
@@ -13,15 +39,32 @@ Um tabuleiro é a conjunção de
     Uma string nome
     dois ints indicando altura e largura 
 */
-typedef struct Tabuleiro_{
-    tpGrafo pGrafo;
+
+typedef struct TAB_tppTabuleiro_{
+    GRA_tppGrafo pGrafo;  
     char* nome;
     int altura;
     int largura;
-} Tabuleiro;
-typedef Tabuleiro * tpTabuleiro;
+} TAB_tppTabuleiro ;
 
-TAB_CondRet TAB_SalvaTabuleiro(tpTabuleiro pTab){
+/***** Protótipos das funções encapsuladas no módulo *****/
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/***************************************************************************
+*
+*  Função: TAB  &Criar Tabuleiro
+*  ****/
+
+TAB_tpCondRet TAB_CriarTabuleiro ( int alt, int lar, char* nome ){
+    TAB_tppTabuleiro * pTab = NULL;
+    int altura, largura;
+    char* nome;
+    // leitura da altura, largura e nome 
+}
+
+/*
+TAB_tpCondRet TAB_SalvaTabuleiro(TAB_tppTabuleiro pTab){
     idAresta = 0
     idVertice = 0
     print(nome)
@@ -40,7 +83,7 @@ TAB_CondRet TAB_SalvaTabuleiro(tpTabuleiro pTab){
                 idAresta++
             exclui vertice
 }
-/*
+
 TRES OPCOES:
 mudar para ter x y
 ======
@@ -106,18 +149,20 @@ mapa1
 
 
 */
+/*
 
-TAB_CondRet TAB_CarregaTabuleiro(tpTabuleiro pTab){}
+    TAB_tpCondRet TAB_CarregaTabuleiro(TAB_tppTabuleiro pTab){}
 
-//Antes de salvar valida as regras do tabuleiro
-//ve se existe inicial, final e se existe apenas 1 destes
-TAB_CondRet TAB_ValidaTabuleiro(tpTabuleiro pTab){}
-
-TAB_CondRet TAB_CriaTabuleiro(tpTabuleiro* pTab, char* nome,int lar, int alt){}
-//valida se eh possivel se mover do corrente para o proximo
-//ver se eh vizinho, se ta dentro do tabuleiro, se nao eh parede(se existe o vertice)
-TAB_CondRet TAB_ValidaMovimento(tpTabuleiro pTab,int idProximo){}
-//valida se eh possivel alteraro valor do corrente para valor
-TAB_CondRet TAB_ValidaAlteracao(tpTabuleiro pTab,int valor){}
+    Antes de salvar valida as regras do tabuleiro
+    ve se existe inicial, final e se existe apenas 1 destes
+    TAB_tpCondRet TAB_ValidaTabuleiro(tpTabuleiro pTab){}
 
 
+    valida se eh possivel se mover do corrente para o proximo
+    ver se eh vizinho, se ta dentro do tabuleiro, se nao eh parede(se existe o vertice)
+    TAB_tpCondRet TAB_ValidaMovimento(tpTabuleiro pTab,int idProximo){}
+
+    valida se eh possivel alterar o valor do corrente para valor
+    TAB_tpCondRet TAB_ValidaAlteracao(tpTabuleiro pTab,int valor){}
+
+*/

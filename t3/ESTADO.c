b6b6e12577801f1a01f00eace0f6adf194a61b7b
Kaplan, L <contato.leonardokaplan@gmail.com>
2014-10-24 21:50:10 -0200

    .hs

--- t3/ESTADO.c
+++ t3/ESTADO.c
@@ -1,6 +1,6 @@
-
+#include "GRAFO.h"
 //struct para manter o estado do jogo. 
-//Ela é passada para as callbacks para ser modificada e lida.
+//Ela é passada por referência para para ser modificada e lida.
 //Ela é UNICA.
 typedef struct estado_{
     //o id do corrente de Menus eh o o id do menu atual
@@ -9,19 +9,34 @@ typedef struct estado_{
     //id do ultimo menu, do menu pai/acima
     //variavel utilizada para implementar a opcao 0 dos menus
     int UltimoMenu;
-} estado;
-//tipo exportado
-//usado por LEI 
-typedef estado* tppEstado;
+} tpEstado;
 
 
-tppEstado CriaEstado(){
-    malloc(sizeof(estado));
-    GRA_tppGrafo Menus = GRA_CriarGrafo(MEN_DestruirMenu);
-    //GRA_tppGrafo Tabuleiro = GRA_CriarGrafo(MEN_DestruirMenu);
+EST_tppEstado CriaEstado(){
     
-    if(e == NULL||Menus==NULL){
-        APR_Erro(CondRetFaltouMemoria);
-        return 0;
+    EST_tppEstado e = malloc(sizeof(tpEstado));
+    
+    if(e==NULL) 
+    {
+        return NULL;
     }
+    
+    GRA_tppGrafo Menus = GRA_CriarGrafo(MEN_DestruirMenu);
+    
+    if(Menus==NULL)
+    {
+        free(e);
+        return NULL;
+    }
+    e->Menus = Menus;
+    e->UltimoMenu = 0;
+    return e;
+}
+
+EST_CondRet EST_MenuInicial(EST_tppEstado e){
+    //algo proximo a isso, tem q tratar os condret
+    return GRA_MudarCorrente(e->Menus,1);
+}
+int EST_MenuCorrente(EST_tppEstado e){
+    return GRA_ObterIdCorrente(e->Menus);
 }

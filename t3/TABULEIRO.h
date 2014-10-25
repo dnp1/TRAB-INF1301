b3e599565f18b987a6a3507582b611ca5a345d46
eaverdeja <eaverdeja@gmail.com>
2014-10-25 21:01:10 -0200

    Atualização do tabuleiro.h

--- t3/TABULEIRO.h
+++ t3/TABULEIRO.h
@@ -38,6 +38,27 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 
 /***********************************************************************
 *
+*  $TC Tipo de dados: TAB Tipo da casa
+*
+*  $ED Descrição do tipo
+*     Representa se a casa é inicio, fim, chão ou parede
+*
+***********************************************************************/
+
+    typedef enum {
+        
+        TAB_TipoCasaInicio ,
+
+        TAB_TipoCasaFim ,
+        
+        TAB_TipoCasaChao,
+        
+        TAB_TipoCasaParede
+    
+    } TAB_TipoCasa ;
+
+/***********************************************************************
+*
 *  $TC Tipo de dados: TAB Condições de retorno
 *
 *
@@ -57,8 +78,14 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
         TAB_CondRetMovimentoInvalido ,
         /* Movimento inválido */
 
-        TAB_CondRetAlteracaoInvalida 
+        TAB_CondRetAlteracaoInvalida , 
         /* Alteração do valor ( vazio/parede/inicio/fim ) inválida */
+
+        TAB_CondRetTabuleiroValido ,
+        /* Tabuleiro respeita as assertivas */
+
+        TAB_CondRetTabuleiroInvalido 
+        /* Tabuleiro viola as assertivas */
 
     } TAB_tpCondRet ;
     
@@ -82,10 +109,46 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 ***********************************************************************/
 
     TAB_tpCondRet TAB_CriarTabuleiro ( TAB_tppTabuleiro pTab , int altura , int largura , char* nome ) ;
+    
+/***********************************************************************
+*
+*  $FC Função: GRA  &Destruir Tabuleiro
+*
+*  $ED Descrição da função
+*     Destroi o tabuleiro
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     GRA_CondRetOK - criou o tabuleiro sem problemas
+*     GRA_CondRetFaltouMemoria - faltou memoria ao criar o tabuleiro
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_DestruirTabuleiro ( TAB_tppTabuleiro pTab ) ;
 
 /***********************************************************************
 *
-*  $FC Função: GRA  &Valida Movimento
+*  $FC Função: TAB &Pode ir pra cima
+*
+*  $ED Descrição da função
+*     Valida a possibilidade de um movimento para o norte.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O movimento é válido; 
+*     TAB_CondRetMovimentoInvalido - O movimento é inválido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_PodeIrPraCima ( TAB_tppTabuleiro pTab );
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &Pode ir pra esquerda
 *
 *  $ED Descrição da função
 *     Valida a possibilidade de um movimento. 
@@ -94,16 +157,16 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *     pTab - ponteiro para o tabuleiro
 *
 *  $FV Valor retornado
-*     GRA_CondRetOK - O movimento é válido; 
-*     GRA_CondRetMovimentoInvalido - O movimento é inválido; 
+*     TAB_CondRetOK - O movimento é válido; 
+*     TAB_CondRetMovimentoInvalido - O movimento é inválido; 
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_ValidaMovimentoNorte ( TAB_tppTabuleiro pTab );
+    TAB_tpCondRet TAB_PodeIrPraEsquerda ( TAB_tppTabuleiro pTab );
 
 /***********************************************************************
 *
-*  $FC Função: GRA  &Valida Movimento
+*  $FC Função: TAB  &Pode ir pra baixo
 *
 *  $ED Descrição da função
 *     Valida a possibilidade de um movimento. 
@@ -112,16 +175,16 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *     pTab - ponteiro para o tabuleiro
 *
 *  $FV Valor retornado
-*     GRA_CondRetOK - O movimento é válido; 
-*     GRA_CondRetMovimentoInvalido - O movimento é inválido; 
+*     TAB_CondRetOK - O movimento é válido; 
+*     TAB_CondRetMovimentoInvalido - O movimento é inválido; 
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_ValidaMovimentoOeste ( TAB_tppTabuleiro pTab );
+    TAB_tpCondRet TAB_PodeIrPraBaixo ( TAB_tppTabuleiro pTab );
 
 /***********************************************************************
 *
-*  $FC Função: GRA  &Valida Movimento
+*  $FC Função: TAB  &Pode ir pra direita
 *
 *  $ED Descrição da função
 *     Valida a possibilidade de um movimento. 
@@ -130,29 +193,102 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *     pTab - ponteiro para o tabuleiro
 *
 *  $FV Valor retornado
-*     GRA_CondRetOK - O movimento é válido; 
-*     GRA_CondRetMovimentoInvalido - O movimento é inválido; 
+*     TAB_CondRetOK - O movimento é válido; 
+*     TAB_CondRetMovimentoInvalido - O movimento é inválido; 
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_ValidaMovimentoSul ( TAB_tppTabuleiro pTab );
+    TAB_tpCondRet TAB_PodeIrPraDireita ( TAB_tppTabuleiro pTab );
+    
+/***********************************************************************
+*
+*  $FC Função: TAB  &Alterar Casa
+*
+*  $ED Descrição da função
+*     Altera o tipo da casa. 
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     x - x da casa a ser modificada
+*     y - y da casa a ser modificada
+*     tipo - novo tipo da casa
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_AlterarCasa ( TAB_tppTabuleiro pTab , int x , int y , TAB_TipoCasa tipo ) ;
+    
+/***********************************************************************
+*
+*  $FC Função: TAB  &Visualizar Tabuleiro
+*
+*  $ED Descrição da função
+*     Visualiza tabuleiro. 
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     pMatriz - ponteiro para a matriz de caracteres
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_VisualizarTabuleiro ( TAB_tppTabuleiro pTab , char*** pMatriz ) ;
 
 /***********************************************************************
 *
-*  $FC Função: GRA  &Valida Movimento
+*  $FC Função: TAB  &Validar Tabuleiro
 *
 *  $ED Descrição da função
-*     Valida a possibilidade de um movimento. 
+*     Valida o tabuleiro. 
 *
 *  $EP Parâmetros
 *     pTab - ponteiro para o tabuleiro
 *
 *  $FV Valor retornado
-*     GRA_CondRetOK - O movimento é válido; 
-*     GRA_CondRetMovimentoInvalido - O movimento é inválido; 
+*     TAB_CondRetOK - O tabuleiro é válido; 
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_ValidaMovimentoLeste ( TAB_tppTabuleiro pTab );
+    TAB_tpCondRet TAB_ValidarTabuleiro ( TAB_tppTabuleiro pTab ) ;
+    
+/***********************************************************************
+*
+*  $FC Função: TAB  &Salvar Tabuleiro
+*
+*  $ED Descrição da função
+*     Salva o tabuleiro em um arquivo.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     saida - ponteiro para o arquivo aonde o tabuleiro vai ser salvo
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_SalvarTabuleiro ( TAB_tppTabuleiro pTab , FILE* saida ) ;
+        
+/***********************************************************************
+*
+*  $FC Função: TAB  &Carregar Tabuleiro
+*
+*  $ED Descrição da função
+*     Carrega o tabuleiro de um arquivo.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     entrada - ponteiro para o arquivo de onde o tabuleiro vai ser lido
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_CarregarTabuleiro ( TAB_tppTabuleiro pTab, FILE* entrada ) ;
 
 #endif

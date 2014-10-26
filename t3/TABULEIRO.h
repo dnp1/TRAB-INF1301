fdd6d1d31ca2b2b703b1941cd274a29c833308aa
eaverdeja <eaverdeja@gmail.com>
2014-10-25 23:22:51 -0200

    .

--- t3/TABULEIRO.h
+++ t3/TABULEIRO.h
@@ -75,6 +75,9 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
         TAB_CondRetFaltouMemoria ,
         /* Faltou memória */
 
+        TAB_CondRetNaoEhCasa ,
+        /* A casa explicitada nao pertence ao tabuleiro */
+
         TAB_CondRetMovimentoInvalido ,
         /* Movimento inválido */
 
@@ -84,8 +87,11 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
         TAB_CondRetTabuleiroValido ,
         /* Tabuleiro respeita as assertivas */
 
-        TAB_CondRetTabuleiroInvalido 
+        TAB_CondRetTabuleiroInvalido ,
         /* Tabuleiro viola as assertivas */
+
+        TAB_CondRetSemSolucao
+        /* Tabuleiro não tem solução */
 
     } TAB_tpCondRet ;
     
@@ -137,8 +143,6 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 *  $EP Parâmetros
 *     pTab - ponteiro para o tabuleiro
-*     x - x da casa corrente
-*     y - y da casa corrente
 *
 *  $FV Valor retornado
 *     TAB_CondRetOK - O movimento é válido; 
@@ -146,7 +150,7 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_PodeIrPraCima ( TAB_tppTabuleiro pTab , int x , int y );
+    TAB_tpCondRet TAB_PodeIrPraCima ( TAB_tppTabuleiro pTab );
 
 /***********************************************************************
 *
@@ -157,8 +161,6 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 *  $EP Parâmetros
 *     pTab - ponteiro para o tabuleiro
-*     x - x da casa corrente
-*     y - y da casa corrente
 *
 *  $FV Valor retornado
 *     TAB_CondRetOK - O movimento é válido; 
@@ -166,7 +168,7 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_PodeIrPraEsquerda ( TAB_tppTabuleiro pTab , int x , int y );
+    TAB_tpCondRet TAB_PodeIrPraEsquerda ( TAB_tppTabuleiro pTab );
 
 /***********************************************************************
 *
@@ -177,8 +179,6 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 *  $EP Parâmetros
 *     pTab - ponteiro para o tabuleiro
-*     x - x da casa corrente
-*     y - y da casa corrente
 *
 *  $FV Valor retornado
 *     TAB_CondRetOK - O movimento é válido; 
@@ -186,7 +186,7 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_PodeIrPraBaixo ( TAB_tppTabuleiro pTab , int x , int y );
+    TAB_tpCondRet TAB_PodeIrPraBaixo ( TAB_tppTabuleiro pTab );
 
 /***********************************************************************
 *
@@ -197,8 +197,6 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 *  $EP Parâmetros
 *     pTab - ponteiro para o tabuleiro
-*     x - x da casa corrente
-*     y - y da casa corrente
 *
 *  $FV Valor retornado
 *     TAB_CondRetOK - O movimento é válido; 
@@ -206,8 +204,76 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 *
 ***********************************************************************/
 
-    TAB_tpCondRet TAB_PodeIrPraDireita ( TAB_tppTabuleiro pTab , int x , int y );
-    
+    TAB_tpCondRet TAB_PodeIrPraDireita ( TAB_tppTabuleiro pTab );
+
+/***********************************************************************
+*
+*  $FC Função: TAB &Andar pra cima
+*
+*  $ED Descrição da função
+*     Muda o corrente para o vizinho ao norte dele.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O movimento é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_AndarPraCima ( TAB_tppTabuleiro pTab );
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &Andar pra esquerda
+*
+*  $ED Descrição da função
+*     Muda o corrente para o vizinho ao oeste dele. 
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O movimento é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_AndarPraEsquerda ( TAB_tppTabuleiro pTab );
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &Andar pra baixo
+*
+*  $ED Descrição da função
+*     Muda o corrente para o vizinho ao sul dele.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O movimento é válido; 
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_AndarPraBaixo ( TAB_tppTabuleiro pTab );
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &Andar pra direita
+*
+*  $ED Descrição da função
+*     Muda o corrente para o vizinho ao leste dele. 
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O movimento é válido;
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_AndarPraDireita ( TAB_tppTabuleiro pTab );
+
 /***********************************************************************
 *
 *  $FC Função: TAB  &Alterar Casa
@@ -298,5 +364,63 @@ typedef struct TAB_tppTabuleiro_* TAB_tppTabuleiro ;
 ***********************************************************************/
 
     TAB_tpCondRet TAB_CarregarTabuleiro ( TAB_tppTabuleiro pTab, FILE* entrada ) ;
+        
+/***********************************************************************
+*
+*  $FC Função: TAB  &Solucionar Tabuleiro
+*
+*  $ED Descrição da função
+*     Soluciona o tabuleiro. A solução é retornada em uma string de caracteres.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     pSolucao - ponteiro para a cadeia de caracteres que representa a solução
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido;
+*     TAB_CondRetSemSolucao - O tabuleiro não possui solução;
+*
+***********************************************************************/
 
+    TAB_tpCondRet TAB_SolucionarTabuleiro ( TAB_tppTabuleiro pTab, char* pSolucao ) ;    
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &Posicao do Jogador
+*
+*  $ED Descrição da função
+*     Da a posição (x,y) do jogador no tabuleiro.
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     pX - ponteiro para inteiro que representa a coordenada X do jogador
+*     pY - ponteiro para inteiro que representa a coordenada Y do jogador
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido;
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_PosicaoJogador ( TAB_tppTabuleiro pTab , int* x , int* y ) ;        
+
+/***********************************************************************
+*
+*  $FC Função: TAB  &getTipoCasa
+*
+*  $ED Descrição da função
+*     Da o tipo da casa referente as coordenadas (x,y)
+*
+*  $EP Parâmetros
+*     pTab - ponteiro para o tabuleiro
+*     x - inteiro que representa a coordenada x
+*     y - inteiro que representa a coordenada y
+*     pTipo - ponteiro para o tipo da casa
+*
+*  $FV Valor retornado
+*     TAB_CondRetOK - O tabuleiro é válido;
+*     TAB_CondRetNaoEhCasa - As coordenadas explicitadas não pertencem ao tabuleiro;
+*
+***********************************************************************/
+
+    TAB_tpCondRet TAB_GetTipoCasa ( TAB_tppTabuleiro pTab, int x , int y , TAB_TipoCasa* pTipo ) ;
 #endif
